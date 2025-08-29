4a2549226248dca88cabf90f1030b53105816419
Navneet-u <navneetupadhyay431@gmail.com>
2025-08-29 12:18:17 +0530

    Add files via upload

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+mkdir -p ~/backup
+
+for f in *.txt
+do
+    name="${f%.txt}"
+    date_time=$(date +"%Y%m%d_%H%M%S")
+    cp "$f" ~/backup/"${name}_$date_time.txt"
+done
+
+echo "Backup Done"
 
